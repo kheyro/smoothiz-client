699857fa6de8cd8b4f7201434f11c8a1e93b0392
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-08 14:32:15 +0200

    [UP] Move routes to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
 
 import Header from './containers/Header';
+import Signup from './containers/Signup';
+import Signin from './containers/auth/Signin';
+
 import './styles/styles.scss';
 
 const App = () => (
   <div>
     <Header />
-    Welcome Home!
+    <Switch>
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+    </Switch>
   </div>
 );
 
