790889314226d7ee3b06455e7b6bf12fd6103f61
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-08 17:20:04 +0200

    [ADD] AUTH_ERROR reducer

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,3 +1,14 @@
-export function auth(state = { auth: false, error: [] }, action) {
-  return state
+import actionTypes from '../actions/actionTypes';
+
+export function auth(state = {}, action) {
+  switch (action.type) {
+    case actionTypes.AUTH_USER:
+      return { ...state, authenticated: true };
+    case actionTypes.UNAUTH_USER:
+      return { ...state, authenticated: false };
+    case actionTypes.AUTH_ERROR:
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
 }
