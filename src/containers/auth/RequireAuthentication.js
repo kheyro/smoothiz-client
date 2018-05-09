f2efe4e7fef374689428448fb22899cc8dd74089
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-09 16:36:07 +0200

    [ADD] create requireAuth HOC to check auth on route

---
+++ src/containers/auth/RequireAuthentication.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: PropTypes.object,
+    };
+    componentWillMount() {
+      this.checkAuth(this.props.authenticated);
+    }
+
+    componentWillUpdate(nextProps) {
+      this.checkAuth(!nextProps.authenticated);
+    }
+
+    checkAuth(isAuthenticated) {
+      if (!isAuthenticated) this.context.router.history.push('/');
+    }
+
+    render() {
+      console.log(this.context)
+      return <ComposedComponent {...this.props} />;
+    }
+  }
+  const mapStateToProps = state => ({
+    authenticated: state.auth.authenticated,
+  });
+
+  return connect(mapStateToProps)(Authentication);
+}
