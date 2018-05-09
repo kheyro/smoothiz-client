8a4931c3427d8decd25f209c5562e8fc9bcb37f6
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-09 11:32:48 +0200

    [ADD] Features routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './containers/Header';
 import Signup from './containers/auth/Signup';
 import Signin from './containers/auth/Signin';
 import Signout from './containers/auth/Signout';
+import Features from './components/Features';
 
 import './styles/styles.scss';
 
@@ -16,6 +17,7 @@ const App = () => (
       <Route path="/signin" component={Signin} />
       <Route path="/signup" component={Signup} />
       <Route path="/signout" component={Signout} />
+      <Route path="/features" component={Features} />
     </Switch>
   </div>
 );
