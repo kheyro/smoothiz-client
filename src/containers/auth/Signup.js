be430797a5f8fcf498e66b3427353b007f3755a7
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-11 15:48:30 +0200

    [ADD] import formValidator helper and create validatation rules

--- src/containers/auth/Signup.js
+++ src/containers/auth/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { signupUser } from '../../actions/authentication';
+import { FormValidator, FVDisplayError } from '../../../helpers/formValidator';
 
 class Signup extends Component {
   constructor() {
@@ -9,15 +10,20 @@ class Signup extends Component {
       email: '',
       password: '',
       passwordConfirm: '',
+      form: {},
     };
   }
 
   onFormSubmit = e => {
     e.preventDefault();
-    this.props.signupUser({
-      email: this.state.email,
-      password: this.state.password,
-    });
+    const form = validation.validate(this.state);
+    this.setState({ form });
+    if (form.isValid) {
+      this.props.signupUser({
+        email: this.state.email,
+        password: this.state.password,
+      });
+    }
   };
 
   handleChange = e =>
@@ -51,6 +57,7 @@ class Signup extends Component {
               type="text"
               value={this.state.email}
             />
+            <FVDisplayError field={this.state.form.email} />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
@@ -62,6 +69,7 @@ class Signup extends Component {
               type="text"
               value={this.state.password}
             />
+            <FVDisplayError field={this.state.form.password} />
           </div>
           <div className="form-group">
             <label htmlFor="passwordConfirm">Confirm Password</label>
@@ -73,6 +81,7 @@ class Signup extends Component {
               type="text"
               value={this.state.passwordConfirm}
             />
+            <FVDisplayError field={this.state.form.passwordConfirm} />
           </div>
           {this.renderAlert()}
           <button type="submit" className="btn btn-primary">Sign up</button>
@@ -85,5 +94,22 @@ class Signup extends Component {
 const mapStateToProps = state => ({
   errorMessage: state.auth.error,
 });
+
+const validation = new FormValidator([
+  {
+    fieldName: 'email',
+    friendlyName: 'email',
+    rules: ['isNotEmpty', 'isEmail'],
+  },
+  {
+    fieldName: 'password',
+    rules: ['isNotEmpty'],
+  },
+  {
+    fieldName: 'passwordConfirm',
+    friendlyName: 'password confirmation',
+    rules: ['isNotEmpty', { equals: ['password'] }],
+  },
+]);
 
 export default connect(mapStateToProps, { signupUser })(Signup);
