72b15535ade09ba6797e41e57a348e621e185730
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-12 17:50:51 +0200

    [UP] add formValidator helper

--- src/containers/auth/Signin.js
+++ src/containers/auth/Signin.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { FormValidator, FVDisplayError } from '../../../helpers/formValidator';
 import { signinUser } from '../../actions/authentication';
 
 class Signin extends Component {
@@ -9,15 +10,20 @@ class Signin extends Component {
     this.state = {
       email: '',
       password: '',
+      form: {},
     };
   }
 
   onFormSubmit = e => {
     e.preventDefault();
-    this.props.signinUser({
-      email: this.state.email,
-      password: this.state.password,
-    });
+    const form = validation.validate(this.state);
+    this.setState({ form });
+    if (form.isValid) {
+      this.props.signinUser({
+        email: this.state.email,
+        password: this.state.password,
+      });
+    }
   };
 
   handleChange = e =>
@@ -51,6 +57,7 @@ class Signin extends Component {
               type="text"
               value={this.state.email}
             />
+            <FVDisplayError field={this.state.form.email} />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
@@ -62,6 +69,7 @@ class Signin extends Component {
               type="password"
               value={this.state.password}
             />
+            <FVDisplayError field={this.state.form.password} />
           </div>
           {this.renderAlert()}
           <button type="submit" className="btn btn-primary">Sign in</button>
@@ -70,6 +78,18 @@ class Signin extends Component {
     );
   }
 }
+
+const validation = new FormValidator([
+  {
+    fieldName: 'email',
+    friendlyName: 'email',
+    rules: ['isRequired', 'isEmail'],
+  },
+  {
+    fieldName: 'password',
+    rules: ['isRequired'],
+  },
+]);
 
 const mapStateToProps = state => ({
   errorMessage: state.auth.error,
