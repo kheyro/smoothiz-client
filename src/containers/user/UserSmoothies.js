45fa17215e0a655d16068342cd9184d8c797581e
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-13 19:05:04 +0200

    [ADD] .DS_STORE file

---
+++ src/containers/user/UserSmoothies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MySmoothies extends Component {
+  render() {
+    return (
+      <div>
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default MySmoothies;
