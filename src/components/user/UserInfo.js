ea61a36335648ccb186a0717b5c1da828aedf96e
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-24 15:35:52 +0200

    [UP] add default picture is no profile picture in db

--- src/components/user/UserInfo.js
+++ src/components/user/UserInfo.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import globals from '../../../config/globals';
 
-const UserInfo = ({ user }) => (
-  <div>
-    <p>
-      {user.firstname} {user.lastname}
-    </p>
-  </div>
-);
+const UserInfo = ({ user }) => {
+  const profilePicture = user.picture
+    ? `${globals.API_SERVER}/profile/r/${user.picture}`
+    : `${globals.API_SERVER}/images/placeholder-profile-200x200.jpg`;
+  return (
+    <div>
+      <div>
+        <img src={profilePicture} alt={user.firstname} />
+      </div>
+      <p>
+        {user.firstname} {user.lastname}
+      </p>
+    </div>
+  );
+};
 
 export default UserInfo;
