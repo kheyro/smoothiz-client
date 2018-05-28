2a0825b4a304f79964fd37c6772015310d7b37be
Denis Atkesone <denis.atkesone@gmail.com>
2018-05-28 17:26:43 +0200

    [RF] Lint

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   renderLinks() {
     if (this.props.auth.authenticated) {
-      return [
-        <li className="nav-item">
-          <Link
-            className="nav-link"
-            to={{ pathname: `/users/${this.props.auth.user.id}` }}
-          >
-            My Smoothies
-          </Link>
-        </li>,
-        <li className="nav-item">
-          <Link className="nav-link" to="/signout">
-            Sign Out
-          </Link>
-        </li>,
-      ];
+      return (
+        <Fragment>
+          <li className="nav-item">
+            <Link
+              className="nav-link"
+              to={{ pathname: `/users/${this.props.auth.user.id}` }}
+            >
+              My Smoothies
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/signout">
+              Sign Out
+            </Link>
+          </li>
+        </Fragment>
+      );
     }
-    return [
-      <li className="nav-item" key={1}>
-        <Link className="nav-link" to="/signin">
-          Sign In
-        </Link>
-      </li>,
-      <li className="nav-item" key={2}>
-        <Link className="nav-link" to="/signup">
-          Sign Up
-        </Link>
-      </li>,
-    ];
+    return (
+      <Fragment>
+        <li className="nav-item">
+          <Link className="nav-link" to="/signin">
+            Sign In
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/signup">
+            Sign Up
+          </Link>
+        </li>
+      </Fragment>
+    );
   }
 
   render() {
@@ -55,6 +60,19 @@ class Header extends Component {
     );
   }
 }
+
+Header.propTypes = {
+  auth: PropTypes.shape({
+    authenticated: PropTypes.bool,
+    user: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }),
+};
+
+Header.defaultProps = {
+  auth: {},
+};
 
 const mapStateToProps = state => ({
   auth: state.auth,
