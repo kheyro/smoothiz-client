4d1467633359e10595102c57ab3b763843374250
Denis Atkesone <denis.atkesone@gmail.com>
2018-06-04 16:29:12 +0200

    [UP] change url depending on mode_env

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,10 @@
+const isProd = () => process.env.NODE_ENV === 'production';
+
 export default {
-  API_SERVER: 'http://localhost:3000',
-  CLIENT_SERVER: 'http://localhost:8080',
+  API_SERVER: isProd()
+    ? 'https://smoothiz-api-server.herokuapp.com'
+    : 'http://localhost:3000',
+  CLIENT_SERVER: isProd()
+    ? 'https://smoothiz.herokuapp.com'
+    : 'http://localhost:8080',
 };
