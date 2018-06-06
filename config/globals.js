0caad1abb22fa0afd058a370b1c3926efb4d3df7
Denis Atkesone <denis.atkesone@gmail.com>
2018-06-06 13:54:20 +0200

    [ADD] add AWS global for smoothie pics

--- config/globals.js
+++ config/globals.js
@@ -7,4 +7,7 @@ export default {
   CLIENT_SERVER: isProd()
     ? 'https://smoothiz.herokuapp.com'
     : 'http://localhost:8080',
+  AWS_BUCKET: isProd()
+    ? 'https://smoothiz.s3.amazonaws.com'
+    : 'https://smoothiz.s3.amazonaws.com/dev',
 };
